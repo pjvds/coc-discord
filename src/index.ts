8913fb7b4f11e6fe2581b332a9714f4be94eba61
Pieter Joost van de Sande <pj@born2code.net>
2022-06-03 12:00:20 +0200

    feat: retry discord client connect

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { workspace } from 'coc.nvim';
 import { Client } from 'discord-rpc';
-import * as O from 'fp-ts/lib/Option';
-import { pipe } from 'fp-ts/lib/pipeable';
 import { Logger } from './utils/logger';
+import retry from 'async-retry';
 
 const clientId: string = '694558978776105000';
 const logger: Logger = new Logger('discord');
@@ -10,7 +8,6 @@ const ELAPSE_UPDATE_DURATION: number = 10000;
 
 const setActivity = (client: Client, startTimestamp: number) => {
   const details = "";
-
   const state = "";
 
   client.setActivity({ state, details, startTimestamp, instance: false });
@@ -18,18 +15,31 @@ const setActivity = (client: Client, startTimestamp: number) => {
 
 const activate = () => {
   logger.info('Starting coc-discord...');
-  const discordRpcClient = new Client({ transport: 'ipc' });
-
-  discordRpcClient.connect(clientId);
-  discordRpcClient.login({ clientId }).catch((e) => logger.error(e));
-
   const startTimestamp = Date.now();
 
+  const discordRpcClient = new Client({ transport: 'ipc' });
   discordRpcClient.on('ready', () => {
     setActivity(discordRpcClient, startTimestamp);
     setInterval(() => setActivity(discordRpcClient, startTimestamp), ELAPSE_UPDATE_DURATION);
     logger.info(`Started coc-discord client. Updating activity every ${ELAPSE_UPDATE_DURATION / 1000}s.`);
   });
+
+  retry(
+    async(bail) => {
+      try {
+        await discordRpcClient.connect( clientId );
+        await discordRpcClient.login({ clientId });
+      } catch (err) {
+        bail(err);
+      }
+    },{
+      retries: 10000000,
+      factor: 3,
+      minTimeout: 1 * 1000,
+      maxTimeout: 15 * 1000,
+      randomize: true,
+    }
+  )
 };
 
 export { activate };
